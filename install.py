0f33fba169b4f48c26781401cb111aa81eec041d
ٱللَّٰه‎➳𝘼Ᏸ𝔇ᴜ͢ŁŁΛH҉ <161955932+Xylon-404@users.noreply.github.com>
2025-06-16 01:23:11 +0600

    Add files via upload

---
+++ install.py
@@ -0,0 +1,13 @@
+import os
+import os
+# Pip install (no -y needed)
+os.system("pip install requests")
+os.system("pip install bs4")
+os.system("pip install colorama")
+os.system("pip install rich")
+# Package install (Termux uses -y)
+os.system("pkg install mpv -y")
+os.system("pkg install ruby -y")
+# gem install lolcat (no -y needed)
+os.system("gem install lolcat")
+os.system("python FB-SHERE.py")
